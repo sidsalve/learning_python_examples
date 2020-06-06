69953c64bf908f328e79f4529a65d44d32c8f9e7
Param-g <gavhadp@gmail.com>
2020-06-06 21:12:23 +0530

    Some String Function

---
+++ stringex.py
@@ -0,0 +1,6 @@
+str = "we Learning Python"
+print("convert first character into upper case",str.capitalize())
+print("Convert string into lower case",str.casefold())
+print("Converts a string into lower case",str.lower())
+print("Converts a string into upper case",str.upper())
+print("Swaps cases, lower case becomes upper case and upper case become lower",str.swapcase())
